9c503248f2123d0dceeb934f06fd2855b18785b8
Jayson-prog <javerdomaraya7@gmail.com>
2026-03-02 16:09:04 +0800

    1234

--- endpoint/settings.py
+++ endpoint/settings.py
@@ -22,9 +22,11 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',  
     
     'handleEndpoint',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -151,6 +153,16 @@ LOGGING = {
         },
     },
 }
+
+# CORS configuration
+# Allow the specific front-end origin (GitHub Pages) or set to True for testing.
+# Prefer explicit origins in production.
+CORS_ALLOWED_ORIGINS = [
+    'https://jaysonpardilla.github.io',
+]
+# If you need to allow the site during testing quickly, you can set:
+# CORS_ALLOW_ALL_ORIGINS = True
+
 
 
 
